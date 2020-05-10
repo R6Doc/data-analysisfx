3d1a2fefb42dee0ca05a28f88bfa4d47f9c1abcd
fx-masterblock <a.ruizg1450@gmail.com>
2020-05-09 20:11:22 -0500

    connected to terminal and account botfx of metatrader5, the file release currency information EUR/USD

--- infoeurusd.py
+++ infoeurusd.py
@@ -6,18 +6,17 @@ print("MetaTrader5 package version: ",mt5.__version__)
 # establish connection to the MetaTrader 5 terminal
 if not mt5.initialize():
     print("initialize() failed, error code =",mt5.last_error())
-    quit()
+    
  
-#base demo account to connect to the terminal
+# connect to the trade account without specifying a password and a server
 account=27865035
 authorized=mt5.login(account)  
 if authorized:
-    print("connected to account #27865035 ".format(account))
+    print("connected to account # ".format(account))
 else:
-    print("failed to connect at account #27865035 , error code:".format(account, mt5.last_error()))
+    print("failed to connect at account # , error code:".format(account, mt5.last_error()))
 
 
- #demo account that we will use for data analysis
     account=28896895
 authorized=mt5.login(account, password="xzrtv8do")
 if authorized:
@@ -28,7 +27,7 @@ if authorized:
     for prop in account_info_dict:
         print("".format(prop, account_info_dict[prop]))
 else:
-    print("failed to connect at account #28896895, error code: ".format(account, mt5.last_error()))
+    print("failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
  
 # attempt to enable the display of the EURJPY symbol in MarketWatch
 selected=mt5.symbol_select("EURUSD",True)
@@ -45,9 +44,9 @@ if symbol_info!=None:
     print("EURUSD: spread =",symbol_info.spread,"  digits =",symbol_info.digits)
     # display symbol properties as a list
     print("Show symbol_info(\"EURUSD\")._asdict():")
-    symbol_info_dict = mt5.symbol_info("EURUSD")._asdict()
+    symbol_info_dict = mt5.symbol_info("EURJPY")._asdict()
     for prop in symbol_info_dict:
-        print("".format(prop, symbol_info_dict[prop]))
+        print("  {}={}".format(prop, symbol_info_dict[prop]))
  
 # shut down connection to the MetaTrader 5 terminal
 mt5.shutdown()
